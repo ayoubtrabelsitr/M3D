7626cadeba7f0e04711d4984fa70d1054c5baac5
ayoubtrabelsitr <ayoubtrabelsitr@gmail.com>
2022-12-15 12:48:42 +0100

    tp6
    tp6

---
+++ src/lab_works/lab_work_6/lab_work_6.cpp
@@ -0,0 +1,182 @@
+#include "glm/gtc/type_ptr.hpp"
+#include "imgui.h"
+#include "lab_work_6.hpp"
+#include "utils/random.hpp" //bib pour getrandomvec3f
+#include "utils/read_file.hpp"
+#include <iostream>
+namespace M3D_ISICG
+{
+	const std::string LabWork6::_shaderFolder = "src/lab_works/lab_work_6/shaders/";
+
+	LabWork6::~LabWork6()
+	{
+		glDeleteProgram( Program ); // Destruction du programme
+		bunny.cleanGL();
+	}
+
+	bool LabWork6::init()
+	{
+		const std::string vertexShaderStr
+			= readFile( _shaderFolder + "meshtexture.vert" ); // lire le shader  et le stocker dans vertexShaderStr
+		const std::string fragmentShaderStr
+			= readFile( _shaderFolder + "meshtexture.frag" );	// lire le shader  et le stocker dans fragmentShaderStr
+		vertexShader	  = glCreateShader( GL_VERTEX_SHADER ); // Construction du vertex shader
+		fragmentShader	  = glCreateShader( GL_FRAGMENT_SHADER ); // Construction du fragment shader
+		const char * VSrc = vertexShaderStr.c_str();	  // variable intermédiaire pour l'utiliser dans glCreateShader
+		const char * FSrc = fragmentShaderStr.c_str();	  // variable intermédiaire pour l'utiliser dans glCreateShader
+		glShaderSource( vertexShader, 1, &VSrc, NULL );	  // Initialisation du code source pour vertex Shader
+		glShaderSource( fragmentShader, 1, &FSrc, NULL ); // Initialisation du code source pour fragment Shader
+		glCompileShader( vertexShader );				  // Compilation pour recupérer les erreurs de compilation
+		glCompileShader( fragmentShader );				  // Compilation pour recupérer les erreurs de compilation
+		std::cout << "Initializing lab work 4..." << std::endl;
+		// Set the color used by glClear to clear the color buffer
+		glClearColor( _bgColor.x, _bgColor.y, _bgColor.z, _bgColor.w );
+
+		GLint compiled;
+		glGetShaderiv( vertexShader, GL_COMPILE_STATUS, &compiled );
+		if ( !compiled )
+		{
+			GLchar log[ 1024 ];
+			glGetShaderInfoLog( vertexShader, sizeof( log ), NULL, log );
+			glDeleteShader( vertexShader );
+			glDeleteShader( fragmentShader );
+			std ::cerr << " Error compiling vertex shader : " << log << std ::endl;
+			return false;
+		}
+		// Check if compilation is ok
+		// fragment.----------------------------------------------------------------------------
+		glGetShaderiv( fragmentShader, GL_COMPILE_STATUS, &compiled );
+		if ( !compiled )
+		{
+			GLchar log[ 1024 ];
+			glGetShaderInfoLog( fragmentShader, sizeof( log ), NULL, log );
+			glDeleteShader( vertexShader );
+			glDeleteShader( fragmentShader );
+			std ::cerr << " Error compiling vertex shader : " << log << std ::endl;
+			return false;
+		}
+
+		Program = glCreateProgram(); // Création du programme
+		// Attachage shaders/programme
+		glAttachShader( Program, vertexShader );
+		glAttachShader( Program, fragmentShader );
+		// Liaison du programme
+		glLinkProgram( Program );
+
+		GLint linked;
+		glGetProgramiv( Program, GL_LINK_STATUS, &linked );
+		if ( !linked )
+		{
+			GLchar log[ 1024 ];
+			glGetProgramInfoLog( Program, sizeof( log ), NULL, log );
+			std ::cerr << " Error linking program : " << log << std ::endl;
+			return false;
+		}
+
+		glUseProgram( Program );
+		_initCamera(); // appel de la fct pour l'initialisation du camera
+
+		// bunny.load( "Bunny", "data/models/bunny2/bunny_2.obj" );
+		bunny.load( "Bunny", "data/models/sponza/sponza.obj" );
+		bunny._transformation = glm::scale( bunny._transformation, glm::vec3( 0.003f ) );
+		////////////////////////////////////variables uniformes //////////////////////////
+		location_MVP		  = glGetUniformLocation( Program, "MVPMatrix" );
+		location_NormalMatrix = glGetUniformLocation( Program, "NormalMatrix" );
+		location_ViewMatrix	  = glGetUniformLocation( Program, "ViewMatrix" );
+		location_ModelMatrix  = glGetUniformLocation( Program, "ModelMatrix" );
+		locationCameraPos	  = glGetUniformLocation( Program, "Camerapos" );
+		///////////////////////////////////////////////////////////////////////////////////
+		glDeleteShader( vertexShader );
+		glDeleteShader( fragmentShader );
+		std::cout << "Done!" << std::endl;
+		return true;
+	}
+
+	void LabWork6::animate( const float p_deltaTime ) {}
+
+	void LabWork6::render()
+	{
+		glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+		glEnable( GL_DEPTH_TEST );
+		/////////////////////////////////Calculer les matrices
+		//////////////////////////////////////////////////////////////////////////
+		MVPa				= camera.getProjectionMatrix() * camera.getViewMatrix() * bunny._transformation;
+		Mat3f Normal_Matrix = Mat3f( glm::transpose( glm::inverse( camera.getViewMatrix() * bunny._transformation ) ) );
+		/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		//
+		//////////////////////Mettre a jour les variables uniformes////////////////////////////////////////
+
+		glProgramUniformMatrix4fv( Program, location_MVP, 1, GL_FALSE, glm::value_ptr( MVPa ) );
+		glProgramUniformMatrix3fv( Program, location_NormalMatrix, 1, GL_FALSE, glm::value_ptr( Normal_Matrix ) );
+		glProgramUniform3fv(
+			Program, glGetUniformLocation( Program, "lightPos" ), 1, glm::value_ptr( Vec3f( 0.f, 0.f, 0.f ) ) );
+		glProgramUniformMatrix4fv(
+			Program, location_ViewMatrix, 1, GL_FALSE, glm::value_ptr( camera.getViewMatrix() ) );
+		glProgramUniformMatrix4fv(
+			Program, location_ModelMatrix, 1, GL_FALSE, glm::value_ptr( bunny._transformation ) );
+		glProgramUniform3fv( Program, locationCameraPos, 1, glm::value_ptr( camera.Positioncamera() ) );
+
+		//////////////////////////////////////////////////////////////////////////////////////////////////
+
+		bunny.render( Program );
+	}
+	void LabWork6::displayUI()
+	{
+		ImGui::Begin( "Settings lab work 5" );
+		ImGui::Text( "No setting available!" );
+		ImGui::End();
+	}
+	void LabWork6::_initCamera()
+	{
+		camera.setPosition( Vec3f( 1.f, 2.f, 0 ) );
+		camera.setScreenSize( _windowWidth, _windowHeight );
+		camera.setLookAt( Vec3f( 0.f, 0.f, 0.f ) );
+	}
+	void LabWork6::_updateViewMatrix()
+	{
+		glProgramUniformMatrix4fv(
+			Program, location_ViewMatrix, 1, GL_FALSE, glm::value_ptr( camera.getViewMatrix() ) );
+	}
+	void LabWork6::handleEvents( const SDL_Event & p_event )
+	{
+		if ( p_event.type == SDL_KEYDOWN )
+		{
+			switch ( p_event.key.keysym.scancode )
+			{
+			case SDL_SCANCODE_W: // Front
+				camera.moveFront( _cameraSpeed );
+				_updateViewMatrix();
+				break;
+			case SDL_SCANCODE_S: // Back
+				camera.moveFront( -_cameraSpeed );
+				_updateViewMatrix();
+				break;
+			case SDL_SCANCODE_A: // Left
+				camera.moveRight( -_cameraSpeed );
+				_updateViewMatrix();
+				break;
+			case SDL_SCANCODE_D: // Right
+				camera.moveRight( _cameraSpeed );
+				_updateViewMatrix();
+				break;
+			case SDL_SCANCODE_R: // Up
+				camera.moveUp( _cameraSpeed );
+				_updateViewMatrix();
+				break;
+			case SDL_SCANCODE_F: // Bottom
+				camera.moveUp( -_cameraSpeed );
+				_updateViewMatrix();
+				break;
+			default: break;
+			}
+		}
+
+		// Rotate when left click + motion (if not on Imgui widget).
+		if ( p_event.type == SDL_MOUSEMOTION && p_event.motion.state & SDL_BUTTON_LMASK
+			 && !ImGui::GetIO().WantCaptureMouse )
+		{
+			camera.rotate( p_event.motion.xrel * _cameraSensitivity, p_event.motion.yrel * _cameraSensitivity );
+			_updateViewMatrix();
+		}
+	}
+} // namespace M3D_ISICG
---
+++ src/lab_works/lab_work_6/lab_work_6.hpp
@@ -0,0 +1,59 @@
+#ifndef __LAB_WORK_6_HPP__
+#define __LAB_WORK_6_HPP__
+#include "GL/gl3w.h"
+#include "common/base_lab_work.hpp"
+#include "common/camera.hpp"
+#include "common/models/triangle_mesh.hpp"
+#include "common/models/triangle_mesh_model.hpp"
+#include "define.hpp"
+#include <vector>
+
+namespace M3D_ISICG
+{
+	class LabWork6 : public BaseLabWork
+	{
+	  public:
+		LabWork6() : BaseLabWork() {}
+		~LabWork6();
+
+		bool init() override;
+		void animate( const float p_deltaTime ) override;
+		void render() override;
+
+		void handleEvents( const SDL_Event & p_event ) override;
+		void displayUI() override;
+
+	  private:
+		// ================ Scene data.
+
+		Camera			  camera;
+		TriangleMeshModel bunny;
+
+		void			  _initCamera();
+		void			  _updateViewMatrix();
+		float			  fovy				 = 60;
+		float			  _cameraSpeed		 = 0.1f;
+		float			  _cameraSensitivity = 0.1f;
+		const std::string vertexShaderStr;	 // Déclaration de variable pour lire et stocker vertex shader
+		const std::string fragmentShaderStr; // Déclaration de variable pour lire et stocker fragment shader
+		// ================ GL data.
+		GLuint vertexShader, fragmentShader; // Déclaration des shaders
+		GLuint Program;						 // pour stocker l’identifiant de l’objet Program
+		GLint  location_MVP;				 // Pour l'adresse de la variable uniforme modelmatrix
+		GLuint location_ViewMatrix, location_ModelMatrix, location_ProjectionMatrix;
+		GLuint location_NormalMatrix;
+		GLuint locationCameraPos;
+		// ================
+		// Mat4f modelView = glm::mat4( 1.f );
+		Mat4f MVPa;
+
+		// ================ Settings.
+		Vec4f _bgColor = Vec4f( 0.8f, 0.8f, 0.8f, 1.f ); // Background color
+
+		// ================
+
+		static const std::string _shaderFolder;
+	};
+} // namespace M3D_ISICG
+
+#endif // __LAB_WORK_6_HPP__
